a55d37a285b9e98514ced1ba3530885279bbf4fc
MathcaTi <tehmatcha28@gmail.com>
2023-05-31 15:12:26 +0800

    refactor dark mode button

--- src/components/common/darkModeBtn.tsx
+++ src/components/common/darkModeBtn.tsx
@@ -5,12 +5,12 @@ import { useEffect, useState } from 'react';
 import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 
 interface IProps {
-  className?: string;
+  visibility?: string;
 }
 
-export default function DarkModeBtn({ className }: IProps) {
+export default function DarkModeBtn({ visibility }: IProps) {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme, systemTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
@@ -20,29 +20,16 @@ export default function DarkModeBtn({ className }: IProps) {
     return null;
   }
 
-  const currentTheme = theme === 'system' ? systemTheme : theme;
-
   return (
     <>
-      {currentTheme === 'dark' ? (
-        <button
-          onClick={() => setTheme('light')}
-          className={`${
-            className ? className + 'hidden' : 'sm:hidden'
-          } dark:bg-dark-main dark:hover:bg-dark-main/80 inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white shadow-lg shadow-slate-200 hover:bg-white/80 dark:shadow-none sm:text-sm xl:inline-flex`}
-        >
-          <BsFillSunFill />
-        </button>
-      ) : (
-        <button
-          onClick={() => setTheme('dark')}
-          className={`${
-            className ? className + 'hidden' : 'sm:hidden'
-          } dark:bg-dark-main dark:hover:bg-dark-main/80 inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white shadow-lg shadow-slate-200 hover:bg-white/80 dark:shadow-none sm:text-sm xl:inline-flex`}
-        >
-          <BsFillMoonFill />
-        </button>
-      )}
+      <button
+        onClick={() => setTheme(theme == 'light' ? 'dark' : 'light')}
+        className={`${
+          visibility && visibility
+        } inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-white shadow-lg shadow-light-accent hover:bg-white/80 dark:bg-dark-main dark:shadow-none dark:hover:bg-dark-main/80 xl:inline-flex`}
+      >
+        {theme == 'light' ? <BsFillMoonFill /> : <BsFillSunFill />}
+      </button>
     </>
   );
 }
