0126f1515fdc86b32dd2e2d70d9f5209437f87cc
MathcaTi <tehmatcha28@gmail.com>
2023-06-01 11:14:31 +0800

    add sidebar section

--- src/components/layout/homeLayout.tsx
+++ src/components/layout/homeLayout.tsx
@@ -1,10 +1,12 @@
 import Navbar from './navbar';
+import Sidebar from './sidebar';
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className='mx-auto w-full max-w-[1440px] xl:px-20 2xl:px-0'>
       <div className='grid w-full px-4 sm:grid-cols-3 sm:pl-20 xl:grid-cols-5 xl:px-0 xl:pl-0'>
         <Navbar />
+        <Sidebar />
         {children}
       </div>
     </section>
---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,80 @@
+'use client';
+import { AiFillHome } from 'react-icons/ai';
+import { BsBookmarkFill, BsFillChatLeftDotsFill, BsFire } from 'react-icons/bs';
+import { IoPersonCircle } from 'react-icons/io5';
+import { usePathname } from 'next/navigation';
+import commitanLogo from '../../../public/images/commitan-logo.svg';
+import Image from 'next/image';
+import DarkModeBtn from '../common/darkModeBtn';
+
+interface MenuItem {
+  href: string;
+  label: string;
+  icon?: React.ReactNode;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    href: '/',
+    label: 'Beranda',
+    icon: <AiFillHome className='text-2xl group-hover:text-commitan-main sm:text-3xl' />,
+  },
+  {
+    href: '/rekomendasi',
+    label: 'Rekomendasi',
+    icon: <BsFire className='text-2xl group-hover:text-commitan-main sm:text-3xl' />,
+  },
+  {
+    href: '/tersimpan',
+    label: 'Tersimpan',
+    icon: <BsBookmarkFill className='text-xl group-hover:text-commitan-main sm:text-2xl' />,
+  },
+  {
+    href: '/chat',
+    label: 'Chat',
+    icon: <BsFillChatLeftDotsFill className='text-xl group-hover:text-commitan-main sm:text-2xl' />,
+  },
+  {
+    href: '/profil',
+    label: 'Profil',
+    icon: <IoPersonCircle className='text-2xl group-hover:text-commitan-main sm:text-3xl xl:inline' />,
+  },
+];
+
+export default function Sidebar() {
+  const pathName = usePathname();
+
+  return (
+    <div className='order-1'>
+      <nav className='fixed bottom-0 left-0 z-[9999] w-full bg-white p-4 px-8 dark:bg-dark-main sm:top-0 sm:w-auto sm:px-4 xl:sticky xl:top-20 xl:bg-transparent xl:p-0 xl:dark:bg-transparent'>
+        <ul className='relative flex items-center justify-between gap-10 sm:h-screen sm:flex-col sm:pt-3 xl:h-auto xl:items-start'>
+          <li className='hidden sm:inline xl:hidden'>
+            <Image src={commitanLogo} alt='Commitan Logo' width={28} height={28} />
+          </li>
+          {menuItems.map(({ label, href, icon }, index) => {
+            return (
+              <li key={index}>
+                <a
+                  href={href}
+                  className={`${
+                    href == pathName && 'common-bg text-commitan-main'
+                  } hover:common-bg flex items-center gap-4 rounded shadow-none duration-100 hover:text-commitan-main hover:shadow-none xl:w-40 xl:px-2 xl:py-1`}
+                >
+                  {icon}
+                  <span className='hidden xl:inline'>{label}</span>
+                </a>
+              </li>
+            );
+          })}
+          <li className='hidden flex-1 sm:inline xl:hidden'>
+            {/* notification */}
+            <div className='flex h-8 w-8 items-center justify-center rounded-full bg-commitan-main'>n</div>
+          </li>
+          <li className='hidden -translate-y-8 sm:inline xl:hidden '>
+            <DarkModeBtn />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
