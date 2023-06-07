fb00424630500c867ed83dc65d15f9a2450f9c8f
MathcaTi <tehmatcha28@gmail.com>
2023-06-07 14:06:37 +0800

    setup temp axios with cors

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,13 @@
+import axios from 'axios';
+
 export function cn(...cns: Array<string>) {
   return cns.join(' ');
 }
+
+export const axiosWithCors = axios.create({
+  baseURL: process.env.API_URL,
+  headers: {
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Content-Type': 'application/json',
+  },
+});
