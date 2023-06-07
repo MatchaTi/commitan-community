cae5c258b8dccb5c637255e9b1f75a99984c58fc
MathcaTi <tehmatcha28@gmail.com>
2023-06-07 14:07:29 +0800

    add modal component

---
+++ src/components/common/modalWrapper.tsx
@@ -0,0 +1,52 @@
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment } from 'react';
+
+interface IWrapper {
+  title: string;
+  children: React.ReactNode;
+  showUploadModal: boolean;
+  toggleUploadModal: () => void;
+}
+
+export default function ModalWrapper({ title, children, showUploadModal, toggleUploadModal }: IWrapper) {
+  return (
+    <>
+      <Transition appear show={showUploadModal} as={Fragment}>
+        <Dialog as='div' className='relative z-[9999]' onClose={toggleUploadModal}>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-75'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-75'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            {/* backdrop */}
+            <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm' />
+          </Transition.Child>
+          <div className='fixed inset-0 top-20 overflow-y-auto px-4'>
+            <div className='flex min-h-full items-start justify-center'>
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel className='common-bg w-full max-w-[800px] transform overflow-y-auto rounded-lg p-4 transition-all'>
+                  <Dialog.Title as='h3' className='text-center text-base font-medium'>
+                    {title}
+                  </Dialog.Title>
+                  <div className='mt-2'>{children}</div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
---
+++ src/components/common/uploadModal.tsx
@@ -0,0 +1,208 @@
+'use client';
+
+import { useUiStore } from '@/stores/uiStore';
+import { useEffect, useRef, useState } from 'react';
+import { shallow } from 'zustand/shallow';
+import Button from './button';
+import CodeEditor from './codeEditor';
+import ModalWrapper from './modalWrapper';
+import OptionButton from './optionButton';
+import { listCategory } from '@/utils/data';
+import ProfileImage from './profileImage';
+
+interface IUpload {
+  title: string;
+  desc: string;
+  category: string;
+  linkSourceCode: string;
+  linkLiveDemo: string;
+}
+
+export default function UploadModal() {
+  const [
+    showUploadModal,
+    toggleShowUploadModal,
+    showLinkSourceCode,
+    toggleSourceCode,
+    showLinkLiveDemo,
+    toggleLiveDemo,
+  ] = useUiStore(
+    (state) => [
+      state.showUploadModal,
+      state.toggleShowUploadModal,
+      state.showLinkSourceCode,
+      state.toggleSourceCode,
+      state.showLinkLiveDemo,
+      state.toggleLiveDemo,
+    ],
+    shallow
+  );
+  const [showCodeEditor, setShowCodeEditor] = useState(false);
+  const [inputUserUpload, setInputUserUpload] = useState<IUpload>({
+    title: '',
+    desc: '',
+    category: 'general',
+    linkSourceCode: '',
+    linkLiveDemo: '',
+  });
+  const [syntax, setSyntax] = useState('');
+  const [pathFile, setPathFile] = useState('');
+  const [heightValue, setHeightValue] = useState('');
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const code = {
+    syntax,
+    pathFile,
+  };
+
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) {
+    const { name, value } = e.target;
+
+    if (name == 'desc') {
+      setInputUserUpload({
+        ...inputUserUpload,
+        [name]: value,
+      });
+      setHeightValue(value);
+    } else {
+      setInputUserUpload({
+        ...inputUserUpload,
+        [name]: value,
+      });
+    }
+  }
+
+  function handleModal() {
+    toggleShowUploadModal(!showUploadModal);
+  }
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [heightValue]);
+
+  function toggleCodeEditor() {
+    setShowCodeEditor(!showCodeEditor);
+    if (!showCodeEditor) {
+      setSyntax('');
+      setPathFile('');
+    }
+  }
+
+  function textOnly() {
+    setShowCodeEditor(false);
+    setSyntax('');
+    setPathFile('');
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    alert('berhasil dikirim');
+  }
+
+  return (
+    <>
+      <Button type='button' onClick={handleModal}>
+        Modal
+      </Button>
+      <ModalWrapper showUploadModal={showUploadModal} title='Buat Postingan' toggleUploadModal={handleModal}>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-2 flex items-center gap-4'>
+            <ProfileImage />
+            <div>
+              <h2 className='text-base font-semibold'>Kumala</h2>
+              <select
+                name='category'
+                id='category'
+                onChange={onChangeHandler}
+                className='common-bg-secondary max-h-28 rounded px-2 py-1 outline-none'
+              >
+                {listCategory.map(({ value, label }, index) => (
+                  <option key={index} value={value}>
+                    {label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <input
+            type='text'
+            placeholder='Judul'
+            name='title'
+            id='title'
+            value={inputUserUpload.title}
+            onChange={onChangeHandler}
+            className='common-bg-secondary mb-2 w-full rounded-lg p-4 text-base font-semibold outline-none'
+            maxLength={96}
+            autoComplete='off'
+            required
+          />
+          <textarea
+            name='desc'
+            id='desc'
+            value={inputUserUpload.desc}
+            onChange={onChangeHandler}
+            ref={textareaRef}
+            placeholder='Bagikan atau tanya sesuatu kepada lainnya!'
+            className='mb-2 max-h-96 w-full resize-none bg-transparent outline-none'
+            autoComplete='off'
+            maxLength={4000}
+          ></textarea>
+          {showCodeEditor && (
+            <CodeEditor
+              context='upload'
+              syntax={syntax}
+              setSyntax={setSyntax}
+              pathFile={pathFile}
+              setPathFile={setPathFile}
+            />
+          )}
+          {showLinkSourceCode && (
+            <input
+              type='text'
+              placeholder='Link Source Code'
+              name='linkSourceCode'
+              id='linkSourceCode'
+              value={inputUserUpload.linkSourceCode}
+              onChange={onChangeHandler}
+              className='mb-2 mt-4 w-full rounded-lg bg-pink-400/25 p-4 text-pink-400 outline-none placeholder:text-pink-800/25 dark:placeholder:text-pink-300/25'
+              autoComplete='off'
+            />
+          )}
+          {showLinkLiveDemo && (
+            <input
+              type='text'
+              placeholder='Link Live Demo'
+              name='linkLiveDemo'
+              id='linkLiveDemo'
+              value={inputUserUpload.linkLiveDemo}
+              onChange={onChangeHandler}
+              className='mb-2 w-full rounded-lg bg-green-400/25 p-4 text-green-400 outline-none placeholder:text-green-800/25 dark:placeholder:text-green-300/25'
+              autoComplete='off'
+            />
+          )}
+          <div className='mt-2 flex items-center justify-between'>
+            <OptionButton
+              context='upload'
+              textOnly={textOnly}
+              showCodeEditor={showCodeEditor}
+              toggleCodeEditor={toggleCodeEditor}
+              showLinkSourceCode={showLinkSourceCode}
+              showLinkLiveDemo={showLinkLiveDemo}
+              toggleLinkSourceCode={toggleSourceCode}
+              toggleLinkLiveDemo={toggleLiveDemo}
+            />
+            <Button
+              type='submit'
+              disabled={!inputUserUpload.title}
+              color={inputUserUpload.title ? 'primary' : 'disabled'}
+            >
+              Kirim
+            </Button>
+          </div>
+        </form>
+      </ModalWrapper>
+    </>
+  );
+}
