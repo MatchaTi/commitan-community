196f9d3fa107eb7f874a86ba2afe02d4dc0839e1
MathcaTi <tehmatcha28@gmail.com>
2023-06-07 17:17:36 +0800

    update post skeleton

--- src/components/skeleton/postSkeleton.tsx
+++ src/components/skeleton/postSkeleton.tsx
@@ -1,36 +1,30 @@
 export default function PostSkeleton() {
   return (
-    <div className='common-bg mt-4 flex h-fit w-full gap-4 rounded-lg p-4'>
-      <div className='h-12 w-12 rounded-full bg-slate-400/25'></div>
-      <div className='flex-1'>
-        <div className='flex w-full animate-pulse items-center gap-4'>
-          <div className='h-6 w-40 rounded-full bg-slate-400/25'></div>
-          <div className='flex-1'></div>
-          <div className='h-6 w-20 rounded-full bg-slate-400/25'></div>
+    <>
+      {[1, 2].map((item) => (
+        <div key={item} className='common-bg mt-4 flex h-fit w-full gap-4 rounded-lg p-4'>
+          <div className='h-12 w-12 rounded-full bg-slate-400/25'></div>
+          <div className='flex-1'>
+            <div className='flex w-full animate-pulse items-center gap-4'>
+              <div className='h-6 w-40 rounded-full bg-slate-400/25'></div>
+              <div className='flex-1'></div>
+              <div className='h-6 w-20 rounded-full bg-slate-400/25'></div>
+            </div>
+            <div className='mt-4 animate-pulse pb-40'>
+              <div className='mb-4 h-4 w-48 rounded-full bg-slate-400/25'></div>
+            </div>
+            <div className='mt-4 flex w-full animate-pulse items-center gap-4'>
+              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
+              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
+              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
+              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
+              <div className='flex-1'></div>
+              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
+            </div>
+            <span className='sr-only'>Loading...</span>
+          </div>
         </div>
-        <div className='mt-4 animate-pulse'>
-          <div className='mb-4 h-4 w-48 rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 w-4/5 rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 w-3/5 rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 max-w-[300px] rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 max-w-[360px] rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 w-4/5 rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 w-3/5 rounded-full bg-slate-400/25'></div>
-          <div className='mb-2.5 h-2 max-w-[300px] rounded-full bg-slate-400/25'></div>
-          <div className='h-2 max-w-[360px] rounded-full bg-slate-400/25'></div>
-        </div>
-        <div className='mt-4 flex w-full animate-pulse items-center gap-4'>
-          <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-          <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-          <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-          <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-          <div className='flex-1'></div>
-          <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-        </div>
-        <span className='sr-only'>Loading...</span>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
