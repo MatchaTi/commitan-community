68bc9f41af59adca478e5bd2eaefc541e11ac53a
MathcaTi <tehmatcha28@gmail.com>
2023-06-07 16:24:15 +0800

    add new component post skeleton and update postlist loader, error message, no post mesage

--- src/components/common/postList.tsx
+++ src/components/common/postList.tsx
@@ -4,6 +4,7 @@ import { usePagination } from '@/libs/hooks';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Post from './post';
 import type { UserPost } from '@/interfaces/post';
+import PostSkeleton from '../skeleton/postSkeleton';
 
 export default function PostList() {
   const { paginatedPosts, isReachingEnd, isLoading, error, mutate, size, setSize } = usePagination<UserPost>(
@@ -11,14 +12,14 @@ export default function PostList() {
   );
   return (
     <div>
-      {error && <div>Terjadi error...</div>}
-      {isLoading && <div>Loading...</div>}
+      {error && <p className='py-4 text-center text-base font-semibold'>Terjadi error ...</p>}
+      {isLoading && <PostSkeleton />}
       {paginatedPosts && (
         <InfiniteScroll
           next={() => setSize(size + 1)}
           hasMore={!isReachingEnd}
-          loader={<p>Loading...</p>}
-          endMessage={<p>No more post...</p>}
+          loader={<PostSkeleton />}
+          endMessage={<p className='py-4 text-center text-base font-semibold'>No more post...</p>}
           dataLength={paginatedPosts.length}
         >
           {paginatedPosts.map((post) => (
