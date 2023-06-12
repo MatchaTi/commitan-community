170546267bac718f7ecfa0523120257bae701cf4
MathcaTi <tehmatcha28@gmail.com>
2023-06-12 19:37:58 +0800

    bug fixed overflow

--- src/components/skeleton/postSkeleton.tsx
+++ src/components/skeleton/postSkeleton.tsx
@@ -1,28 +1,17 @@
 export default function PostSkeleton() {
   return (
     <>
-      {[1, 2].map((item) => (
-        <div key={item} className='common-bg mt-4 flex h-fit w-full gap-4 rounded-lg p-4'>
-          <div className='h-12 w-12 rounded-full bg-slate-400/25'></div>
-          <div className='flex-1'>
-            <div className='flex w-full animate-pulse items-center gap-4'>
-              <div className='h-6 w-40 rounded-full bg-slate-400/25'></div>
-              <div className='flex-1'></div>
-              <div className='h-6 w-20 rounded-full bg-slate-400/25'></div>
-            </div>
-            <div className='mt-4 animate-pulse pb-40'>
-              <div className='mb-4 h-4 w-48 rounded-full bg-slate-400/25'></div>
-            </div>
-            <div className='mt-4 flex w-full animate-pulse items-center gap-4'>
-              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-              <div className='flex-1'></div>
-              <div className='h-8 w-8 rounded-lg bg-slate-400/25'></div>
-            </div>
-            <span className='sr-only'>Loading...</span>
+      {[1, 2, 3].map((item) => (
+        <div key={item} className='common-bg mt-4 h-fit w-full rounded-lg p-4'>
+          <div className='flex items-center gap-4 pb-20'>
+            <div className='h-10 w-10 animate-pulse rounded-full bg-slate-400/25'></div>
+            <div className='h-4 w-20 animate-pulse rounded-full bg-slate-400/25'></div>
           </div>
+          <div className='flex items-center justify-between'>
+            <div className='h-4 w-20 animate-pulse rounded-full bg-slate-400/25'></div>
+            <div className='h-4 w-20 animate-pulse rounded-full bg-slate-400/25'></div>
+          </div>
+          <span className='sr-only'>Loading...</span>
         </div>
       ))}
     </>
