bd14bf344edae06bad1e2969fd3f78104d949dde
MathcaTi <tehmatcha28@gmail.com>
2023-06-13 09:44:31 +0800

    update layout main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import PostList from '@/components/common/postList';
+import UploadModal from '@/components/common/uploadModal';
 import Category from '@/components/layout/category';
 import Following from '@/components/layout/following';
 import HomeLayout from '@/components/layout/homeLayout';
@@ -8,9 +10,11 @@ export default function Home() {
     <HomeLayout>
       <main className='order-2 mx-auto flex min-h-[2000px] w-full pt-16 sm:col-span-4 sm:col-start-1 sm:pt-20 xl:col-span-3 xl:col-start-2'>
         <div className='mx-auto w-full'>
+          <UploadModal />
           <Trending visibility='xl:hidden' />
           <Following visibility='xl:hidden' />
           <Category visibility='xl:hidden' />
+          <PostList />
         </div>
       </main>
     </HomeLayout>
