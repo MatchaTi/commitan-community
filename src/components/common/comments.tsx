6476c28d5e597a516bdb20feba694ee8371bf90c
MathcaTi <tehmatcha28@gmail.com>
2023-06-14 14:01:59 +0800

    add new component comment control, comments, and edit comment modal

---
+++ src/components/common/comments.tsx
@@ -0,0 +1,77 @@
+import { Comment } from '@/interfaces/post';
+import { timeAgo } from '@/utils/utils';
+import { GoPrimitiveDot } from 'react-icons/go';
+import Badge from './badge';
+import CodeEditor from './codeEditor';
+import CommentControl from './commentControl';
+import EditCommentModal from './editCommentModal';
+import ProfileImage from './profileImage';
+import { useState } from 'react';
+
+interface IComment {
+  postId: string;
+  comment: Comment;
+}
+
+export default function Comments({ postId, comment }: IComment) {
+  const [editModal, setEditModal] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
+
+  function handleEditModal() {
+    setEditModal(!editModal);
+  }
+
+  function handleDeleteModal() {
+    setDeleteModal(!deleteModal);
+  }
+
+  return (
+    <div key={comment._id} className='mt-4 flex items-start gap-4 rounded-lg'>
+      {/* left side start */}
+      <div className='flex flex-col items-end'>
+        {/* user image */}
+        <ProfileImage size='medium' />
+        {/* hooks */}
+        <div className='mt-2 h-8 w-8 translate-x-2 border-b-4 border-l-4 border-light-accent dark:border-dark-accent '></div>
+      </div>
+      {/* left side end */}
+      {/* right side start */}
+      <div className='w-full'>
+        {/* top information */}
+        <div className='flex items-start justify-between'>
+          <div className='mb-2'>
+            <div className='flex items-center gap-2'>
+              <h2 className='font-semibold'>{comment.username}</h2>
+              <GoPrimitiveDot />
+              <time>{timeAgo(comment.createdAt)}</time>
+            </div>
+            <Badge>Wengdev</Badge>
+          </div>
+          <CommentControl handleEditModal={handleEditModal} handleDeleteModal={handleDeleteModal} />
+          {editModal && (
+            <EditCommentModal
+              postId={postId}
+              comment={comment}
+              showEditCommentModal={editModal}
+              handleEditCommentModal={handleEditModal}
+            />
+          )}
+        </div>
+        {/* bottom content */}
+        <div className='mt-2'>
+          <div className='mb-4 rounded-lg bg-light-secondary p-4 dark:bg-dark-secondary'>
+            <p>{comment.text}</p>
+            {comment.code && (
+              <>
+                {comment.code.syntax && comment.code.pathFile ? (
+                  <CodeEditor context='commented' syntax={comment.code.syntax} pathFile={comment.code.pathFile} />
+                ) : null}
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* right side end */}
+    </div>
+  );
+}
---
+++ src/components/common/editCommentModal.tsx
@@ -0,0 +1,156 @@
+'use client';
+
+import { Comment } from '@/interfaces/post';
+import { useUserPostStore } from '@/stores/postsStore';
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+import { shallow } from 'zustand/shallow';
+import Button from './button';
+import CodeEditor from './codeEditor';
+import ModalWrapper from './modalWrapper';
+import OptionButton from './optionButton';
+import ProfileImage from './profileImage';
+import Spinner from './spinner';
+
+interface IEdit {
+  text: string;
+}
+
+interface IEditComment {
+  postId: string;
+  comment: Comment;
+  showEditCommentModal: boolean;
+  handleEditCommentModal: () => void;
+}
+
+export default function EditCommentModal({
+  postId,
+  comment,
+  showEditCommentModal,
+  handleEditCommentModal,
+}: IEditComment) {
+  const [editComment] = useUserPostStore((state) => [state.editComment], shallow);
+  const [showCodeEditor, setShowCodeEditor] = useState(false);
+  const [inputUserEdit, setInputUserEdit] = useState<IEdit>({
+    text: comment.text,
+  });
+  const [syntax, setSyntax] = useState(comment.code?.syntax);
+  const [pathFile, setPathFile] = useState(comment.code?.pathFile);
+  const [isLoading, setIsLoading] = useState(false);
+  const [heightValue, setHeightValue] = useState('');
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const code = {
+    syntax,
+    pathFile,
+  };
+
+  function onChangeHandler(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    const { name, value } = e.target;
+    setInputUserEdit({
+      ...inputUserEdit,
+      [name]: value,
+    });
+    setHeightValue(value);
+  }
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [heightValue]);
+
+  function toggleCodeEditor() {
+    setShowCodeEditor(!showCodeEditor);
+    if (!showCodeEditor) {
+      setSyntax('');
+      setPathFile('');
+    }
+  }
+
+  function textOnly() {
+    setShowCodeEditor(false);
+    setSyntax('');
+    setPathFile('');
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+    const username = 'Kumala';
+    await new Promise((r) => setTimeout(r, 3000));
+    try {
+      const res = await axios.patch(`${process.env.API_URL}/posts/${postId}/comments/edit/${comment._id}`, {
+        username,
+        ...inputUserEdit,
+        code,
+      });
+      setIsLoading(false);
+      const updatedComment = res.data.comment;
+      editComment(postId, comment._id, updatedComment);
+      handleEditCommentModal();
+    } catch (error) {
+      // action error
+      console.log(error, 'error');
+    }
+  }
+
+  return (
+    <>
+      {showEditCommentModal && (
+        <ModalWrapper showModal={showEditCommentModal} title='Edit Komentar' toggleModal={handleEditCommentModal}>
+          <form onSubmit={handleSubmit}>
+            <div className='mb-2 flex items-center gap-4'>
+              <ProfileImage />
+              <div>
+                <h2 className='text-base font-semibold'>{comment.username}</h2>
+              </div>
+            </div>
+            <textarea
+              name='text'
+              id='text'
+              value={inputUserEdit.text}
+              onChange={onChangeHandler}
+              ref={textareaRef}
+              placeholder='Ketik komentar anda disini...'
+              className='mb-2 max-h-96 w-full resize-none bg-transparent outline-none'
+              autoComplete='off'
+              maxLength={4000}
+            ></textarea>
+            {showCodeEditor || (syntax && pathFile) ? (
+              <CodeEditor
+                context='editComment'
+                syntax={syntax!}
+                setSyntax={setSyntax}
+                pathFile={pathFile!}
+                setPathFile={setPathFile}
+              />
+            ) : null}
+            <div className='mt-2 flex items-center justify-between'>
+              <OptionButton
+                context='editComment'
+                textOnly={textOnly}
+                showCodeEditor={showCodeEditor}
+                toggleCodeEditor={toggleCodeEditor}
+              />
+              <Button
+                type='submit'
+                disabled={!inputUserEdit.text || isLoading}
+                color={!inputUserEdit.text ? 'disabled' : isLoading ? 'loading' : 'primary'}
+              >
+                {isLoading ? (
+                  <div className='flex items-center gap-2'>
+                    <Spinner size='sm' width='light' />
+                    <span>Loading...</span>
+                  </div>
+                ) : (
+                  'Kirim'
+                )}
+              </Button>
+            </div>
+          </form>
+        </ModalWrapper>
+      )}
+    </>
+  );
+}
