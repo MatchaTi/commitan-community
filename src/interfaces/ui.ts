a14ddc6c7e72fe95441d942e5ba2d9859345e50c
MathcaTi <tehmatcha28@gmail.com>
2023-06-16 19:37:28 +0800

    delete 2 state

--- src/interfaces/ui.ts
+++ src/interfaces/ui.ts
@@ -1,10 +1,4 @@
 export interface UiStore {
   showUploadModal: boolean;
   toggleShowUploadModal: (showUploadModal: boolean) => void;
-
-  showLinkSourceCode: boolean;
-  toggleSourceCode: (showLinkSourceCode: boolean) => void;
-
-  showLinkLiveDemo: boolean;
-  toggleLiveDemo: (showLinkLiveDemo: boolean) => void;
 }
--- src/stores/uiStore.ts
+++ src/stores/uiStore.ts
@@ -4,10 +4,4 @@ import type { UiStore } from '@/interfaces/ui';
 export const useUiStore = create<UiStore>()((set) => ({
   showUploadModal: false,
   toggleShowUploadModal: (showUploadModal) => set(() => ({ showUploadModal })),
-
-  showLinkSourceCode: false,
-  toggleSourceCode: (showLinkSourceCode) => set(() => ({ showLinkSourceCode })),
-
-  showLinkLiveDemo: false,
-  toggleLiveDemo: (showLinkLiveDemo) => set(() => ({ showLinkLiveDemo })),
 }));
