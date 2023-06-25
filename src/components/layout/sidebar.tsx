92355f005ecbfaed8c883299ddd81ee9d9ac600c
MathcaTi <tehmatcha28@gmail.com>
2023-06-25 11:30:26 +0800

    update sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,42 +1,51 @@
 'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { AiFillHome } from 'react-icons/ai';
 import { BsBookmarkFill, BsFillChatLeftDotsFill, BsFire } from 'react-icons/bs';
+import { IoIosNotifications } from 'react-icons/io';
 import { IoPersonCircle } from 'react-icons/io5';
-import { usePathname } from 'next/navigation';
 import commitanLogo from '../../../public/images/commitan-logo.svg';
-import Image from 'next/image';
+import Button from '../common/button';
 import DarkModeBtn from '../common/darkModeBtn';
-import Link from 'next/link';
 
 interface MenuItem {
   href: string;
+  order: string;
   label: string;
   icon?: React.ReactNode;
 }
 
 const menuItems: MenuItem[] = [
   {
-    href: '/',
+    href: '',
+    order: 'order-1',
     label: 'Beranda',
     icon: <AiFillHome className='text-2xl group-hover:text-commitan-main sm:text-3xl' />,
   },
   {
-    href: '/rekomendasi',
+    href: 'rekomendasi',
+    order: 'order-2',
     label: 'Rekomendasi',
     icon: <BsFire className='text-2xl group-hover:text-commitan-main sm:text-3xl' />,
   },
   {
-    href: '/tersimpan',
+    href: 'tersimpan',
+    order: 'order-3',
     label: 'Tersimpan',
     icon: <BsBookmarkFill className='text-xl group-hover:text-commitan-main sm:text-2xl' />,
   },
   {
-    href: '/chat',
+    href: 'chat',
+    order: 'order-4',
     label: 'Chat',
     icon: <BsFillChatLeftDotsFill className='text-xl group-hover:text-commitan-main sm:text-2xl' />,
   },
   {
-    href: '/profil',
+    href: 'profil',
+    order: 'order-6',
     label: 'Profil',
     icon: <IoPersonCircle className='text-2xl group-hover:text-commitan-main sm:text-3xl xl:inline' />,
   },
@@ -44,6 +53,7 @@ const menuItems: MenuItem[] = [
 
 export default function Sidebar() {
   const pathName = usePathname();
+  const splitedPath = pathName.split('/');
 
   return (
     <div className='order-1'>
@@ -52,25 +62,27 @@ export default function Sidebar() {
           <li className='hidden sm:inline xl:hidden'>
             <Image src={commitanLogo} alt='Commitan Logo' width={28} height={28} />
           </li>
-          {menuItems.map(({ label, href, icon }, index) => {
+          {menuItems.map(({ label, href, icon, order }, index) => {
             return (
-              <li key={index}>
-                <Link
-                  href={href}
-                  className={`${
-                    href == pathName && 'text-commitan-main'
-                  } flex items-center gap-4 duration-100 hover:text-commitan-main xl:w-40`}
-                >
+              <li
+                key={index}
+                className={`${
+                  href == splitedPath[1] && 'text-commitan-main'
+                } ${order} flex items-center gap-4 duration-100 hover:text-commitan-main`}
+              >
+                <Link href={`/${href}`} className='flex gap-4 xl:w-40'>
                   {icon} <span className='hidden xl:inline'>{label}</span>
                 </Link>
               </li>
             );
           })}
-          <li className='hidden flex-1 sm:inline xl:hidden'>
+          <li className='order-5 hidden flex-1 sm:inline xl:hidden'>
             {/* notification */}
-            <div className='flex h-8 w-8 items-center justify-center rounded-full bg-commitan-main'>n</div>
+            <Button corner='full' size='sm' color='transparent'>
+              <IoIosNotifications className='text-3xl' />
+            </Button>
           </li>
-          <li className='hidden -translate-y-8 sm:inline xl:hidden '>
+          <li className='order-7 hidden -translate-y-8 sm:inline xl:hidden '>
             <DarkModeBtn />
           </li>
         </ul>
