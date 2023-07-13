c1210c9c2047c29b11d69d5d26ec3a0d969eb271
MathcaTi <tehmatcha28@gmail.com>
2023-07-13 11:54:44 +0800

    update profileSection

--- src/app/profil/[username]/profileSection.tsx
+++ src/app/profil/[username]/profileSection.tsx
@@ -2,6 +2,7 @@
 
 import Badge from '@/components/common/badge';
 import Button from '@/components/common/button';
+import PostList from '@/components/common/postList';
 import ProfileImage from '@/components/common/profileImage';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
@@ -16,32 +17,31 @@ export default function ProfileSection() {
   return (
     <div className='mx-auto w-full pt-6'>
       {/* top information start */}
-      <div className='mb-4 flex items-center justify-between'>
-        {/* left side start */}
-        <div className='flex flex-col gap-4 sm:flex-row sm:items-center'>
-          <ProfileImage size='lg' />
+      <div className='mb-4 space-y-4'>
+        <div className='flex flex-wrap items-center gap-4'>
           <div>
-            <h2 className='text-xl font-semibold capitalize'>{username[2]}</h2>
-            <div className='flex flex-wrap items-center gap-3'>
-              <Badge>Wengdev</Badge>
-              <div className='flex items-center gap-2 text-xs sm:text-sm'>
-                <AiFillCalendar />
-                <span>Bergabung 25 Juni 2023</span>
-              </div>
-            </div>
+            <ProfileImage size='lg' username={username[2]} />
           </div>
-        </div>
-        {/* left side end */}
-        {/* right side start */}
-        <div className='-translate-y-10 sm:translate-y-0'>
-          <Button type='button' color='outline'>
+          <h2 className='flex-1 text-xl font-semibold capitalize'>{username[2]}</h2>
+          <Button type='button' size='md' corner='md' color='outline'>
             Edit Profil
           </Button>
         </div>
-        {/* right side end */}
+        <div className='flex flex-wrap items-center gap-3'>
+          <Badge>Wengdev</Badge>
+          <div className='flex items-center gap-2 text-xs sm:text-sm'>
+            <AiFillCalendar />
+            <span>Bergabung 25 Juni 2023</span>
+          </div>
+        </div>
+        <p className='text-slate-400 dark:text-slate-300'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, magnam nulla. Iusto laborum quaerat a
+          reiciendis corrupti praesentium eligendi totam porro repudiandae voluptates! Ut ea praesentium maiores
+          officiis aliquid eius!
+        </p>
       </div>
       <div className='mb-6'>
-        <div className='mb-4 flex w-full items-center gap-4 border-b-2 border-light-accent pb-4 dark:border-dark-accent'>
+        <div className='mb-4 flex w-full items-center gap-4 border-b-2 border-dark-accent/10 pb-4 text-slate-400 dark:border-light-accent/5 dark:text-slate-300'>
           <span>0 Followers</span>
           <span>0 Following</span>
         </div>
@@ -56,7 +56,7 @@ export default function ProfileSection() {
             onClick={() => setContent(1)}
             color='transparent'
             size='none'
-            className={`${content == 1 ? 'opacity-100' : 'opacity-50'} flex items-center gap-2`}
+            className={`${content == 1 ? 'opacity-100' : 'opacity-50'}`}
           >
             <BsGrid3X3 />
             <span className='uppercase'>Postingan</span>
@@ -66,7 +66,7 @@ export default function ProfileSection() {
             onClick={() => setContent(2)}
             color='transparent'
             size='none'
-            className={`${content == 2 ? 'opacity-100' : 'opacity-50'} flex items-center gap-2`}
+            className={`${content == 2 ? 'opacity-100' : 'opacity-50'}`}
           >
             <BsBookmark />
             <span className='uppercase'>Tersimpan</span>
@@ -75,7 +75,7 @@ export default function ProfileSection() {
       </div>
       {/* top information end */}
       {/* bottom content start */}
-      <div>{content == 1 ? 'hello' : 'world'}</div>
+      <div>{content == 1 ? <PostList /> : 'world'}</div>
       {/* bottom content end */}
     </div>
   );
