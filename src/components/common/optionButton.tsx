3b453ac2686b505163b7f79b156b20eb9f3a32b0
MathcaTi <tehmatcha28@gmail.com>
2023-07-13 09:23:19 +0800

    update component add tooltip

--- src/components/common/optionButton.tsx
+++ src/components/common/optionButton.tsx
@@ -1,6 +1,7 @@
-import Button from './button';
 import { BiCodeCurly, BiEdit } from 'react-icons/bi';
 import { HiCode, HiExternalLink } from 'react-icons/hi';
+import Button from './button';
+import Tooltip from './tooltip';
 
 interface OptionContext {
   upload: string;
@@ -33,51 +34,81 @@ export default function OptionButton({
   toggleLinkLiveDemo,
 }: IOption) {
   return (
-    <div className={`${context == 'speedDial' && 'flex-col-reverse gap-4 text-white'} flex items-center`}>
-      <Button
-        type='button'
-        size='sm'
-        color='transparent'
-        onClick={textOnly}
-        className={`${context == 'speedDial' && 'animate-speed-dial'} group flex items-center hover:text-purple-400`}
-      >
-        <BiEdit className='rounded-lg p-1 text-3xl group-hover:bg-purple-400/25' />
-      </Button>
-      <Button
-        type='button'
-        size='sm'
-        color='transparent'
-        onClick={toggleCodeEditor}
-        className={`${showCodeEditor && 'text-sky-500'} ${
-          context == 'speedDial' && 'animate-speed-dial'
-        } group flex items-center hover:text-sky-400`}
-      >
-        <HiCode className={`${showCodeEditor && 'bg-sky-400/25'} rounded-lg p-1 text-3xl group-hover:bg-sky-400/25`} />
-      </Button>
+    <div
+      className={`${
+        context == 'speedDial' && 'flex-col-reverse gap-4 text-white'
+      } flex items-center text-slate-400 dark:text-slate-300`}
+    >
+      <div className='group relative'>
+        <Button
+          type='button'
+          size='sm'
+          color='transparent'
+          onClick={textOnly}
+          className={`${context == 'speedDial' && 'animate-speed-dial'} group flex items-center hover:text-purple-400`}
+        >
+          <BiEdit className='rounded-lg p-1 text-3xl group-hover:bg-purple-400/25' />
+        </Button>
+        <Tooltip position={context == 'speedDial' ? 'left' : 'topStart'}>
+          <span className='flex w-full items-center justify-center font-semibold'>Hanya Teks</span>
+        </Tooltip>
+      </div>
+      <div className='group relative'>
+        <Button
+          type='button'
+          size='sm'
+          color='transparent'
+          onClick={toggleCodeEditor}
+          className={`${showCodeEditor && 'text-sky-500'} ${
+            context == 'speedDial' && 'animate-speed-dial'
+          } group flex items-center hover:text-sky-400`}
+        >
+          <HiCode
+            className={`${showCodeEditor && 'bg-sky-400/25'} rounded-lg p-1 text-3xl group-hover:bg-sky-400/25`}
+          />
+        </Button>
+        <Tooltip position={context == 'speedDial' ? 'left' : 'topStart'}>
+          <span className='flex w-full items-center justify-center font-semibold'>Code Editor</span>
+        </Tooltip>
+      </div>
       {context == 'upload' || context == 'edit' ? (
         <>
-          <Button
-            type='button'
-            size='sm'
-            color='transparent'
-            onClick={() => toggleLinkSourceCode?.(!showLinkSourceCode)}
-            className={`${showLinkSourceCode && 'text-pink-500'} group flex items-center hover:text-pink-400`}
-          >
-            <BiCodeCurly
-              className={`${showLinkSourceCode && 'bg-pink-400/25'} rounded-lg p-1 text-3xl group-hover:bg-pink-400/25`}
-            />
-          </Button>
-          <Button
-            type='button'
-            size='sm'
-            color='transparent'
-            onClick={() => toggleLinkLiveDemo?.(!showLinkLiveDemo)}
-            className={`${showLinkLiveDemo && 'text-green-500'} group flex items-center hover:text-green-400`}
-          >
-            <HiExternalLink
-              className={`${showLinkLiveDemo && 'bg-green-400/25'} rounded-lg p-1 text-3xl group-hover:bg-green-400/25`}
-            />
-          </Button>
+          <div className='group relative'>
+            <Button
+              type='button'
+              size='sm'
+              color='transparent'
+              onClick={() => toggleLinkSourceCode?.(!showLinkSourceCode)}
+              className={`${showLinkSourceCode && 'text-pink-500'} group flex items-center hover:text-pink-400`}
+            >
+              <BiCodeCurly
+                className={`${
+                  showLinkSourceCode && 'bg-pink-400/25'
+                } rounded-lg p-1 text-3xl group-hover:bg-pink-400/25`}
+              />
+            </Button>
+            <Tooltip position='topStart'>
+              <span className='flex w-full items-center justify-center font-semibold'>Link Source Code</span>
+            </Tooltip>
+          </div>
+          <div className='group relative'>
+            <Button
+              type='button'
+              size='sm'
+              color='transparent'
+              onClick={() => toggleLinkLiveDemo?.(!showLinkLiveDemo)}
+              className={`${showLinkLiveDemo && 'text-green-500'} group flex items-center hover:text-green-400`}
+            >
+              <HiExternalLink
+                className={`${
+                  showLinkLiveDemo && 'bg-green-400/25'
+                } rounded-lg p-1 text-3xl group-hover:bg-green-400/25`}
+              />
+            </Button>
+            <Tooltip position='topStart'>
+              <span className='flex w-full items-center justify-center font-semibold'>Link Live Demo</span>
+            </Tooltip>
+          </div>
         </>
       ) : null}
     </div>
--- src/components/common/postActionButton.tsx
+++ src/components/common/postActionButton.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link';
 import { AiOutlineHeart } from 'react-icons/ai';
 import { BiCommentDetail } from 'react-icons/bi';
 import { BsBookmark } from 'react-icons/bs';
 import { FiSend } from 'react-icons/fi';
 import { HiExternalLink } from 'react-icons/hi';
 import Button from './button';
+import Tooltip from './tooltip';
 
 interface IPostAction {
   postId: string;
@@ -15,40 +15,66 @@ interface IPostAction {
 
 export default function PostActionButton({ postId, isCommentOpen, toggleCommentSection, lengthComment }: IPostAction) {
   return (
-    <div className='mt-2 flex w-full items-center'>
+    <div className='mt-2 flex w-full items-center text-slate-400 dark:text-slate-300'>
       <div className='flex flex-1 items-center gap-1 sm:gap-4'>
-        <Button
-          type='button'
-          size='sm'
-          color='transparent'
-          className='group flex items-center gap-1 hover:text-pink-400'
-        >
-          <AiOutlineHeart className='rounded-lg p-1 text-3xl group-hover:bg-pink-400/25' /> <span>699</span>
-        </Button>
-        <Button
-          type='button'
-          size='sm'
-          color='transparent'
-          onClick={toggleCommentSection}
-          className={`${isCommentOpen && 'text-green-400'} group flex items-center gap-1 hover:text-green-400`}
-        >
-          <BiCommentDetail
-            className={`${isCommentOpen && 'bg-green-400/25'} rounded-lg p-1 text-3xl group-hover:bg-green-400/25`}
-          />
-          <span>{lengthComment}</span>
-        </Button>
-        <Button type='button' size='sm' color='transparent' className='group hover:text-sky-400'>
-          <FiSend className='rounded-lg p-1 text-3xl group-hover:bg-sky-400/25' />
-        </Button>
-        <Link href={`/posts/${postId}`}>
-          <Button type='button' size='sm' color='transparent' className='group hover:text-orange-400'>
-            <HiExternalLink className='rounded-lg p-1 text-3xl group-hover:bg-orange-400/25' />
+        <div className='group relative'>
+          <Button
+            type='button'
+            size='sm'
+            color='transparent'
+            className='group flex items-center gap-1 hover:text-pink-400'
+          >
+            <AiOutlineHeart className='rounded-lg p-1 text-3xl group-hover:bg-pink-400/25' />{' '}
+            <span>{lengthComment}</span>
           </Button>
-        </Link>
+          <Tooltip position='topStart'>
+            <span className='flex w-full items-center justify-center font-semibold'>Suka</span>
+          </Tooltip>
+        </div>
+        <div className='group relative'>
+          <Button
+            type='button'
+            size='sm'
+            color='transparent'
+            onClick={toggleCommentSection}
+            className={`${isCommentOpen && 'text-green-400'} group flex items-center gap-1 hover:text-green-400`}
+          >
+            <BiCommentDetail
+              className={`${isCommentOpen && 'bg-green-400/25'} rounded-lg p-1 text-3xl group-hover:bg-green-400/25`}
+            />
+            <span>{lengthComment}</span>
+          </Button>
+          <Tooltip position='topStart'>
+            <span className='flex w-full items-center justify-center font-semibold'>Komentar</span>
+          </Tooltip>
+        </div>
+        <div className='group relative'>
+          <Button type='button' size='sm' color='transparent' className='group hover:text-sky-400'>
+            <FiSend className='rounded-lg p-1 text-3xl group-hover:bg-sky-400/25' />
+          </Button>
+          <Tooltip position='topStart'>
+            <span className='flex w-full items-center justify-center font-semibold'>Bagikan</span>
+          </Tooltip>
+        </div>
+        <div className='group relative'>
+          <a href={`/posts/${postId}`}>
+            <Button type='button' size='sm' color='transparent' className='group hover:text-orange-400'>
+              <HiExternalLink className='rounded-lg p-1 text-3xl group-hover:bg-orange-400/25' />
+            </Button>
+          </a>
+          <Tooltip position='topStart'>
+            <span className='flex w-full items-center justify-center font-semibold'>Detail</span>
+          </Tooltip>
+        </div>
       </div>
-      <Button type='button' size='sm' color='transparent' className='group hover:text-yellow-400'>
-        <BsBookmark className='rounded-lg p-1 text-3xl group-hover:bg-yellow-400/25' />
-      </Button>
+      <div className='group relative'>
+        <Button type='button' size='sm' color='transparent' className='group hover:text-yellow-400'>
+          <BsBookmark className='rounded-lg p-1 text-3xl group-hover:bg-yellow-400/25' />
+        </Button>
+        <Tooltip position='topEnd'>
+          <span className='flex w-full items-center justify-center font-semibold'>Simpan</span>
+        </Tooltip>
+      </div>
     </div>
   );
 }
