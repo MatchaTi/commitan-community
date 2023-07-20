fa9f0fb22aad3d2d222ff21594d151612cd10f11
MathcaTi <tehmatcha28@gmail.com>
2023-07-21 06:36:33 +0800

    update no-scrollbar

--- src/components/layout/aside.tsx
+++ src/components/layout/aside.tsx
@@ -6,7 +6,7 @@ import Trending from './trending';
 export default function Aside() {
   return (
     <div className='order-3 pl-4 sm:mb-10 sm:pb-20 sm:pt-20'>
-      <aside className='hidden max-h-screen w-full overflow-y-auto pb-10 xl:sticky xl:right-0 xl:top-[70px] xl:block'>
+      <aside className='no-scrollbar hidden max-h-screen w-full overflow-y-auto pb-10 xl:sticky xl:right-0 xl:top-[70px] xl:block'>
         <Trending visibility='hidden xl:block' />
         <Following visibility='hidden xl:block' />
         <Category visibility='hidden xl:block' />
