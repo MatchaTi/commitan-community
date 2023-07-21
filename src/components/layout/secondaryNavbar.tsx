657efbd0ac9b8eee1556179fd68819a2851af858
MathcaTi <tehmatcha28@gmail.com>
2023-07-21 15:39:57 +0800

    update secondaryNavbar

--- src/components/layout/secondaryNavbar.tsx
+++ src/components/layout/secondaryNavbar.tsx
@@ -13,8 +13,8 @@ export default function SecondaryNavbar() {
 
   return (
     <>
-      <div className='fixed left-0 right-0 top-4 z-[99] flex w-full items-center justify-between px-4 xl:hidden'>
-        <div className='xl:hidden'>
+      <div className='fixed left-0 right-0 top-4 z-[99] flex w-full items-center justify-between px-4 lg:hidden'>
+        <div className='lg:hidden'>
           <Image src={CommitanIcon} alt='Commitan Icon' width={28} height={28} />
         </div>
         <Button type='button' color='transparent' onClick={() => setIsClicked(!isClicked)}>
@@ -24,20 +24,23 @@ export default function SecondaryNavbar() {
       <nav
         className={`${
           isClicked ? 'left-0' : '-left-full'
-        } common-bg fixed top-0 z-50 mx-auto h-screen w-full max-w-[1440px] border-none shadow-none backdrop-blur-sm duration-300 ease-in-out xl:left-0 xl:right-0 xl:h-auto xl:bg-transparent xl:px-20 xl:dark:bg-transparent 2xl:px-0`}
+        } common-bg fixed top-0 z-50 mx-auto h-screen w-full max-w-[1440px] border-none shadow-none backdrop-blur-sm duration-300 ease-in-out lg:left-0 lg:right-0 lg:h-auto lg:bg-transparent lg:dark:bg-transparent xl:px-20 2xl:px-0`}
       >
-        <div className='flex h-full w-full flex-col items-center justify-center gap-8 px-4 py-4 xl:flex-row xl:justify-between xl:gap-0 xl:px-0'>
-          <div className='hidden xl:block xl:w-1/6'>
-            <Image src={CommitanIcon} alt='Commitan Icon' width={28} height={28} />
+        <div className='flex h-full w-full flex-col items-center justify-center gap-8 px-4 py-4 lg:flex-row lg:justify-between lg:gap-0 xl:px-0'>
+          <div className='hidden items-center gap-4 lg:flex xl:w-1/6'>
+            <div>
+              <Image src={CommitanIcon} alt='Commitan Icon' width={28} height={28} />
+            </div>
+            <div className='headings text-xl font-bold'>Commitan.</div>
           </div>
-          <ul className='paragraphs flex flex-col items-start justify-center gap-4 xl:flex-1 xl:flex-row xl:items-center xl:gap-8'>
-            <li className='underline-offset-8 duration-150 ease-in-out hover:text-light-headings hover:underline hover:dark:text-dark-headings'>
+          <ul className='paragraphs flex flex-col items-start justify-center gap-4 lg:flex-1 lg:flex-row lg:items-center lg:gap-8'>
+            <li className='text-xl underline-offset-8 duration-150 ease-in-out hover:text-light-headings hover:underline hover:dark:text-dark-headings sm:text-base lg:text-sm'>
               <a href='/'>Beranda</a>
             </li>
             {infoItems.map(({ label, href }, i) => (
               <li
                 key={i}
-                className='underline-offset-8 duration-150 ease-in-out hover:text-light-headings hover:underline hover:dark:text-dark-headings'
+                className='text-xl underline-offset-8 duration-150 ease-in-out hover:text-light-headings hover:underline hover:dark:text-dark-headings sm:text-base lg:text-sm'
               >
                 <a href={href}>{label}</a>
               </li>
