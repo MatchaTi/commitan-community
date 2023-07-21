5dc8d80677ea6b28fa7d4c6b2e20095d88b35444
MathcaTi <tehmatcha28@gmail.com>
2023-07-21 14:06:34 +0800

    add new component secondaryLayout

---
+++ src/components/layout/secondaryLayout.tsx
@@ -0,0 +1,10 @@
+import SecondaryNavbar from './secondaryNavbar';
+
+export default function SecondaryLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className='mx-auto w-full max-w-[1440px] px-4 pt-16 xl:px-20 2xl:px-0'>
+      <SecondaryNavbar />
+      {children}
+    </section>
+  );
+}
