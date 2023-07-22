8c6e8e4d95c1de8febdba0f41f6408181cbc30de
MathcaTi <tehmatcha28@gmail.com>
2023-07-22 22:58:04 +0800

    auth handling profile page

--- src/app/profil/[username]/page.tsx
+++ src/app/profil/[username]/page.tsx
@@ -1,6 +1,17 @@
+import { verifyToken } from '@/libs/auth';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import ProfileSection from './profileSection';
 
 export default function Profile() {
+  const nextCookie = cookies();
+
+  const token = nextCookie.get('token');
+
+  const payload: any = token && verifyToken(token.value);
+
+  if (payload && payload.error) redirect('/auth/login');
+
   return (
     <main className='order-2 mx-auto flex min-h-[2000px] w-full pt-16 sm:col-span-4 sm:col-start-1 sm:pt-20 xl:col-span-3 xl:col-start-2'>
       <ProfileSection />
