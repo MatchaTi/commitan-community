8a8761079c614f550a2bac07ec3b2feeaaad99f3
MathcaTi <tehmatcha28@gmail.com>
2023-07-23 19:24:53 +0800

    add new component footer & update secondaryLayout

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className='common-accent mt-10 w-full border-t bg-light-main dark:bg-dark-main'>
+      <div className='mx-auto w-full max-w-[1440px] px-4 py-4 text-center xl:px-20 2xl:px-0'>
+        <p>&copy; 2023 Commitan oleh Tim Commitan</p>
+      </div>
+    </footer>
+  );
+}
--- src/components/layout/secondaryLayout.tsx
+++ src/components/layout/secondaryLayout.tsx
@@ -1,10 +1,12 @@
+import Footer from './footer';
 import SecondaryNavbar from './secondaryNavbar';
 
 export default function SecondaryLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section className='mx-auto w-full max-w-[1440px] px-4 pb-10 pt-16 xl:px-20 2xl:px-0'>
+    <>
       <SecondaryNavbar />
-      {children}
-    </section>
+      <section className='mx-auto w-full max-w-[1440px] px-4 pb-10 pt-16 xl:px-20 2xl:px-0'>{children}</section>
+      <Footer />
+    </>
   );
 }
