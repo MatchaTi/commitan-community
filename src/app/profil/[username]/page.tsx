8d45bb98d2300a4e59a1bef1828b57ffd55204f8
MathcaTi <tehmatcha28@gmail.com>
2023-07-23 16:24:56 +0800

    update protect page

--- src/app/profil/[username]/page.tsx
+++ src/app/profil/[username]/page.tsx
@@ -1,16 +1,11 @@
-import { verifyToken } from '@/libs/auth';
-import { cookies } from 'next/headers';
+import { useAuthVerify } from '@/hooks/protectPage';
 import { redirect } from 'next/navigation';
 import ProfileSection from './profileSection';
 
 export default function Profile() {
-  const nextCookie = cookies();
+  const { isAuthenticated } = useAuthVerify();
 
-  const token = nextCookie.get('token');
-
-  const payload: any = token && verifyToken(token.value);
-
-  if (payload && payload.error) redirect('/auth/login');
+  if (!isAuthenticated) redirect('/auth/login');
 
   return (
     <main className='order-2 mx-auto flex min-h-[2000px] w-full pt-16 sm:col-span-4 sm:col-start-1 sm:pt-20 xl:col-span-3 xl:col-start-2'>
