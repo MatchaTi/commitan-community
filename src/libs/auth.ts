2a74ef38b3c62c17cbe6e32dd91195652003eba9
MathcaTi <tehmatcha28@gmail.com>
2023-07-26 19:21:40 +0800

    replace jsonwebtoken with jose

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -8,16 +8,22 @@ import {
   VerifyResponse,
 } from '@/interfaces/auth';
 import axios, { AxiosError } from 'axios';
-import jwt from 'jsonwebtoken';
+import { jwtVerify } from 'jose';
 
-export function verifyToken(token: string) {
+export function getJwtSecretKey() {
+  const secret = process.env.JWT_SECRET;
+
+  if (!secret || secret.length === 0) throw new Error('ENV JWT_SECRET is not set.');
+
+  return secret;
+}
+
+export async function verifyToken(token: string) {
   try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    return decodedToken;
+    const decodedToken = await jwtVerify(token, new TextEncoder().encode(getJwtSecretKey()));
+    return decodedToken.payload;
   } catch (error) {
-    return {
-      error,
-    };
+    return { error };
   }
 }
 
@@ -25,7 +31,7 @@ export async function login({ email, password }: ILogin): Promise<LoginResponse>
   try {
     const res = await axios.post(`${process.env.API_URL}/auth/login`, { email, password });
     const { message, token } = res.data;
-    const decodedToken: any = verifyToken(token);
+    const decodedToken: any = await verifyToken(token);
 
     if (decodedToken.error) throw 'Invalid Token';
     return { message, decodedToken, token };
@@ -63,7 +69,7 @@ export async function otpVerify({ email, otp_number }: IVerify): Promise<VerifyR
   try {
     const res = await axios.post(`${process.env.API_URL}/auth/otp-verify`, { email, otp_number });
     const { message, token } = res.data;
-    const decodedToken: any = verifyToken(token);
+    const decodedToken: any = await verifyToken(token);
 
     if (decodedToken.error) throw 'Invalid Token';
     return { message, token };
