d238edd7bf78f266f8d4331694b6a0cba7638572
MathcaTi <tehmatcha28@gmail.com>
2023-07-26 19:20:28 +0800

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { verifyToken } from './libs/auth';
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
-  const token = request.cookies.get('token');
+  const token = request.cookies.get('token')?.value;
 
-  if (request.nextUrl.pathname.startsWith('/profil')) {
-    if (!token) return NextResponse.redirect(new URL('/auth/login', request.url));
+  const verifiedToken =
+    token &&
+    (await verifyToken(token).catch(() => {
+      return NextResponse.redirect(new URL('/auth/login', request.nextUrl.origin));
+    }));
+
+  if (request.nextUrl.pathname.startsWith('/auth') && !verifiedToken) return;
+
+  if (request.url.includes('/profil') && !verifiedToken) {
+    return NextResponse.redirect(new URL('/auth/login', request.nextUrl.origin));
   }
 
-  if (request.nextUrl.pathname.startsWith('/auth')) {
-    if (token) return NextResponse.redirect(new URL(request.nextUrl.host));
+  if (request.url.includes('/auth') && verifiedToken) {
+    return NextResponse.redirect(new URL(request.nextUrl.origin));
   }
 
-  if (request.nextUrl.pathname.startsWith('/rekomendasi')) {
-    if (!token) return NextResponse.redirect(new URL('/auth/login', request.url));
+  if (!verifiedToken) {
+    return NextResponse.redirect(new URL('/auth/login', request.nextUrl.origin));
   }
 
   return response;
 }
+
+export const config = {
+  matcher: ['/auth/:path*', '/profil/:path*', '/rekomendasi', '/tersimpan/:path*'],
+};
