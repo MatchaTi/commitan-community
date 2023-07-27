061bf4af61b6465b2f1a53b0c99891afa43f0527
MathcaTi <tehmatcha28@gmail.com>
2023-07-27 09:35:51 +0800

    refactor sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -7,45 +7,38 @@ import { BsBookmarkFill, BsFillChatLeftDotsFill, BsFire } from 'react-icons/bs';
 import { IoPersonCircle } from 'react-icons/io5';
 import commitanLogo from '../../../public/images/commitan-logo.svg';
 import DarkModeBtn from '../common/darkModeBtn';
-import Notification from '../common/notification';
 
 interface MenuItem {
   href: string;
-  order: string;
   label: string;
   icon?: React.ReactNode;
 }
 
 const menuItems: MenuItem[] = [
   {
-    href: '',
-    order: 'order-1',
+    href: '/',
     label: 'Beranda',
-    icon: <AiFillHome className='text-2xl group-hover:text-commitan-main sm:text-xl xl:text-3xl' />,
+    icon: <AiFillHome className='text-2xl sm:text-xl' />,
   },
   {
     href: 'rekomendasi',
-    order: 'order-2',
     label: 'Rekomendasi',
-    icon: <BsFire className='text-2xl group-hover:text-commitan-main sm:text-xl xl:text-3xl' />,
+    icon: <BsFire className='text-2xl sm:text-xl' />,
   },
   {
     href: 'tersimpan',
-    order: 'order-3',
     label: 'Tersimpan',
-    icon: <BsBookmarkFill className='text-xl group-hover:text-commitan-main sm:text-lg xl:text-2xl' />,
+    icon: <BsBookmarkFill className='text-xl sm:text-lg' />,
   },
   {
     href: 'chat',
-    order: 'order-4',
     label: 'Chat',
-    icon: <BsFillChatLeftDotsFill className='text-xl group-hover:text-commitan-main sm:text-lg xl:text-2xl' />,
+    icon: <BsFillChatLeftDotsFill className='text-xl sm:text-lg' />,
   },
   {
     href: 'profil',
-    order: 'order-6',
     label: 'Profil',
-    icon: <IoPersonCircle className='text-2xl group-hover:text-commitan-main sm:text-3xl' />,
+    icon: <IoPersonCircle className='text-2xl sm:-translate-x-0.5' />,
   },
 ];
 
@@ -54,34 +47,28 @@ export default function Sidebar() {
   const splitedPath = pathName.split('/');
 
   return (
-    <div className='order-1'>
-      <nav className='common-bg fixed bottom-0 left-0 z-[9999] w-full p-4 px-8 shadow-none sm:top-0 sm:w-auto sm:px-2 xl:sticky xl:top-20 xl:border-none xl:bg-transparent xl:p-0 xl:dark:bg-transparent'>
-        <ul className='relative flex items-center justify-between gap-4 sm:h-screen sm:flex-col sm:gap-10 sm:pt-3 xl:h-auto xl:items-start'>
-          <li className='hidden sm:inline xl:hidden'>
+    <>
+      <nav className='group fixed bottom-0 left-0 z-50 w-full overflow-hidden text-xs duration-200 ease-in-out sm:bottom-auto sm:top-0 sm:h-screen sm:w-[3.35rem] sm:hover:w-40'>
+        <ul className='common-bg flex h-full w-full items-center justify-between p-4 sm:flex-col sm:items-start sm:justify-start sm:gap-6 sm:p-1.5'>
+          <li className='hidden h-10 w-10 items-center justify-center sm:flex'>
             <Image src={commitanLogo} alt='Commitan Logo' width={28} height={28} />
           </li>
-          {menuItems.map(({ label, href, icon, order }, index) => {
-            return (
-              <li
-                key={index}
-                className={`${
-                  href == splitedPath[1] && 'text-commitan-main'
-                } ${order} duration-100 hover:text-commitan-main`}
-              >
-                <a href={`/${href}`} className='flex items-center gap-4 xl:w-40'>
-                  {icon} <span className='hidden xl:inline'>{label}</span>
-                </a>
-              </li>
-            );
-          })}
-          <li className='order-5 hidden flex-1 sm:inline xl:hidden'>
-            <Notification />
-          </li>
-          <li className='order-7 hidden -translate-y-8 sm:inline xl:hidden '>
-            <DarkModeBtn />
+          {menuItems.map(({ href, label, icon }, i) => (
+            <a
+              href={href}
+              key={i}
+              className='common-accent relative flex min-w-max items-center gap-2 overflow-hidden duration-200 ease-in-out after:absolute after:bottom-0 after:w-full after:-translate-x-[150%] after:border-b after:border-light-text after:duration-300 after:ease-in-out dark:border-dark-accent dark:border-light-accent/5 after:dark:border-dark-text sm:border-b sm:p-2.5 sm:hover:after:translate-x-0 sm:group-hover:px-4'
+            >
+              <div className={`${splitedPath[1] == href && 'text-commitan-main'}`}>{icon}</div>
+              <div className='hidden hover:text-commitan-main sm:group-hover:block'>{label}</div>
+            </a>
+          ))}
+          <li className='hidden flex-1 sm:block'></li>
+          <li className='hidden w-full sm:block sm:pb-2'>
+            <DarkModeBtn context='sidebar' />
           </li>
         </ul>
       </nav>
-    </div>
+    </>
   );
 }
