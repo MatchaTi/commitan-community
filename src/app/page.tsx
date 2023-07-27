247db610d6441cf6fd188cc93d6f55679da4cc51
MathcaTi <tehmatcha28@gmail.com>
2023-07-27 10:37:33 +0800

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,12 @@ import Category from '@/components/layout/category';
 import Following from '@/components/layout/following';
 import Information from '@/components/layout/information';
 import MainLayout from '@/components/layout/mainLayout';
-import Navbar from '@/components/layout/navbar';
-import Sidebar from '@/components/layout/sidebar';
 import Trending from '@/components/layout/trending';
 
 export default function Home() {
   return (
     <div className='min-h-[2000px]'>
       <MainLayout>
-        <Navbar />
-        <Sidebar />
         <UploadModal />
         <aside className='no-scrollbar h-fit w-full gap-4 overflow-y-auto sm:fixed sm:left-0 sm:top-20 sm:grid sm:h-screen sm:max-h-screen sm:grid-cols-12 sm:px-4 sm:pb-40 lg:px-0'>
           <div className='space-y-4 sm:col-span-4 sm:col-start-9 lg:col-span-2 lg:col-start-10 lg:row-start-1'>
--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -1,3 +1,12 @@
+import Navbar from './navbar';
+import Sidebar from './sidebar';
+
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <main className='grid gap-4 px-4 pb-10 sm:grid-cols-12 lg:px-0'>{children}</main>;
+  return (
+    <main className='grid gap-4 px-4 pb-10 sm:grid-cols-12 lg:px-0'>
+      <Navbar />
+      <Sidebar />
+      {children}
+    </main>
+  );
 }
