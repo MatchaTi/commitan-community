b3f6c1113f470852fa348cc4bd0da6cd4f76c938
MathcaTi <tehmatcha28@gmail.com>
2023-07-27 16:58:28 +0800

    update footer information

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,14 @@
 export default function Footer() {
   return (
     <footer className='common-accent mt-10 w-full border-t bg-light-main dark:bg-dark-main'>
-      <div className='mx-auto w-full max-w-[1440px] px-4 py-4 text-center xl:px-20 2xl:px-0'>
+      <div className='mx-auto flex w-full max-w-[1440px] flex-wrap items-center justify-between px-4 py-4 xl:px-20 2xl:px-0'>
+        <a
+          href='https://github.com/MatchaTi/commitan-community/issues'
+          target={'_blank'}
+          className='underline-offset-4 hover:underline'
+        >
+          Diskusi dengan Kami di Github
+        </a>
         <p>&copy; 2023 Commitan oleh Tim Commitan</p>
       </div>
     </footer>
