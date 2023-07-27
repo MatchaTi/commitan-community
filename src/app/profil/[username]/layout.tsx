f62c84f817008501caeb3f9c021dca6e1a79a928
MathcaTi <tehmatcha28@gmail.com>
2023-07-27 16:33:53 +0800

    update layout profile

--- src/app/profil/[username]/layout.tsx
+++ src/app/profil/[username]/layout.tsx
@@ -1,5 +1,4 @@
-import Navbar from '@/components/layout/navbar';
-import Sidebar from '@/components/layout/sidebar';
+import MainLayout from '@/components/layout/mainLayout';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -9,14 +8,8 @@ export const metadata: Metadata = {
 
 export default function ProfileLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <section className='mx-auto w-full max-w-[1440px] xl:px-20 2xl:px-0'>
-        <div className='grid w-full px-4 sm:grid-cols-3 sm:pl-20 xl:grid-cols-5 xl:px-0 xl:pl-0'>
-          <Navbar />
-          <Sidebar />
-          {children}
-        </div>
-      </section>
+    <div className='min-h-[2000px]'>
+      <MainLayout>{children}</MainLayout>
     </div>
   );
 }
