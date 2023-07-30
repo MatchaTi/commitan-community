dba3bf67881a21ab1fe94087b993d187bcb0fc32
MathcaTi <tehmatcha28@gmail.com>
2023-07-31 06:56:01 +0800

    fixed responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,20 @@ export default function Home() {
   return (
     <div className='min-h-[2000px]'>
       <MainLayout>
-        <div className='sm:col-span-7 sm:col-start-2 xl:col-span-5 xl:col-start-3'>
+        <div className='order-1 sm:col-span-11 sm:col-start-2 xl:col-span-6 xl:col-start-3'>
           <UploadModal />
         </div>
-        <aside className='no-scrollbar h-fit w-full gap-4 overflow-y-auto sm:fixed sm:left-0 sm:top-20 sm:grid sm:h-screen sm:max-h-screen sm:grid-cols-11 sm:px-4 sm:pb-40 xl:px-0 xl:pl-[3.35rem]'>
-          <div className='space-y-4 sm:col-span-3 sm:col-start-9 xl:col-span-2 xl:col-start-8 xl:row-start-1'>
-            <Trending />
-            <Following />
-            <Category />
-            <Information visibility='sm:block hidden' />
+        <section className='order-2 sm:col-span-11 sm:col-start-2 xl:fixed xl:left-0 xl:top-0 xl:order-3 xl:mt-20 xl:grid xl:grid-cols-12 xl:gap-4 xl:pl-[3.35rem]'>
+          <div className='no-scrollbar xl:col-span-2 xl:col-start-9 xl:max-h-screen xl:overflow-auto xl:pb-20'>
+            <div className='space-y-4'>
+              <Trending />
+              <Following />
+              <Category />
+              <Information visibility='hidden xl:block' />
+            </div>
           </div>
-        </aside>
-        <div className='sm:col-span-7 sm:col-start-2 xl:col-span-5 xl:col-start-3'>
+        </section>
+        <div className='order-3 h-[2000px] sm:col-span-11 sm:col-start-2 xl:order-2 xl:col-span-6 xl:col-start-3'>
           <PostList />
         </div>
       </MainLayout>
