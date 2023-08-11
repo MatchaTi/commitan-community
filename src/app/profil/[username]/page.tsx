3c83d525a48341a35a8aa5a1a99c3ad06910efe4
MuhammadRadifa <m.difa123456@gmail.com>
2023-08-11 23:07:55 +0800

    feat (profil) : fetch data profile

--- src/app/profil/[username]/page.tsx
+++ src/app/profil/[username]/page.tsx
@@ -1,12 +1,54 @@
 // import ProfileSection from './profileSection';
+'use client';
 
 import Badge from '@/components/common/badge';
 import Button from '@/components/common/button';
 import Achievement from '@/components/layout/achievement';
 import { AiFillCalendar } from 'react-icons/ai';
 import ProfileSection from './profileSection';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import Cookies from 'js-cookie';
+import moment from 'moment';
+
+interface IProfileUser<T> {
+  _id: T;
+  fullname: T;
+  username: T;
+  job: T;
+  followers: Array<T>;
+  following: Array<T>;
+  createdAt: T;
+  bio: T;
+}
 
 export default function Profile() {
+  const params = useParams();
+  const [data, setData] = useState<IProfileUser<string>>({
+    _id: '',
+    fullname: '',
+    username: '',
+    job: '',
+    followers: [],
+    following: [],
+    createdAt: '',
+    bio: '',
+  });
+  useEffect(() => {
+    const token = Cookies.get('token');
+    const getdata = async () => {
+      const response = await axios.get(`${process.env.API_URL}/user/profile/${params.username}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setData(response.data.data[0]);
+    };
+    getdata();
+  }, []);
+
   return (
     <>
       <section className='order-1 mt-32 sm:col-span-11 sm:col-start-2 xl:col-span-8 xl:col-start-3 xl:mt-20'>
@@ -15,14 +57,14 @@ export default function Profile() {
             <div className='h-40 w-40 rounded-full bg-red-500'></div>
             <div className='space-y-2'>
               <div className='flex flex-wrap items-center gap-4'>
-                <span className='headings text-2xl font-bold'>AdiIfai</span>
-                <Badge>Wengdev</Badge>
+                <span className='headings text-2xl font-bold'>{data.fullname}</span>
+                <Badge>{data.job}</Badge>
               </div>
               <div className='flex flex-wrap items-center gap-4'>
-                <span>@adiifai9</span>
+                <span>{data.username}</span>
                 <div className='flex items-center gap-4'>
                   <AiFillCalendar />
-                  <span>Bergabung 30 Juli 2023</span>
+                  <span>Bergabung {moment(data.createdAt).format('DD MMMM YYYY')}</span>
                 </div>
               </div>
             </div>
@@ -35,26 +77,23 @@ export default function Profile() {
         </div>
         <div className='flex items-center gap-4'>
           <div className='space-x-2'>
-            <span className='headings font-semibold'>99</span>
+            <span className='headings font-semibold'>{data.followers.length}</span>
             <span>Followers</span>
           </div>
           <div className='space-x-2'>
-            <span className='headings font-semibold'>99</span>
+            <span className='headings font-semibold'>{data.following.length}</span>
             <span>Following</span>
           </div>
         </div>
       </section>
       <section className='order-3 h-[2000px] sm:col-span-11 sm:col-start-2 xl:order-2 xl:col-span-6 xl:col-start-3'>
-        <ProfileSection />
+        <ProfileSection id={data._id} />
       </section>
       <section className='order-2 sm:col-span-11 sm:col-start-2 xl:order-3 xl:col-span-2 xl:col-start-9'>
         <div className='xl:sticky xl:top-20 xl:z-10 xl:h-screen xl:overflow-auto'>
           <div className='common-bg w-full rounded-md p-4 leading-normal'>
             <h4 className='mb-4 text-base font-bold text-commitan-main'>Bio</h4>
-            <q className='italic'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum alias aspernatur modi doloremque architecto
-              deserunt exercitationem, et voluptatibus eligendi? Dolore temporibus reiciendis voluptas saepe impedit,
-            </q>
+            <q className='italic'>{data.bio}</q>
           </div>
           <Achievement />
         </div>
