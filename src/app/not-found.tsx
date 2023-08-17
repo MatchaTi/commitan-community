6d8d492e4ec1d0f630dc0e56341c7d09cea7625d
MathcaTi <tehmatcha28@gmail.com>
2023-08-17 10:53:27 +0800

    feat(comp): add not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <>
+      <title>Not Found</title>
+      <div className='flex h-screen w-full items-center justify-center'>Not Found</div>
+    </>
+  );
+}
